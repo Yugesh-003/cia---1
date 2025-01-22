9877406c9e520038353ff9e8e31c75616138bd47
yugesh <a.yugesh2@gmail.com>
2025-01-23 02:20:58 +0530

    added dict programs

---
+++ dict/13.dictionary.py
@@ -0,0 +1,6 @@
+#Python program to access elements from a dictionary
+myDict= {'Name':'Sean', 'Age':24,'Hobby':'Dancing','City':'NY'}
+
+print(myDict['Name'])
+print(myDict['Age'])
+print(myDict['Hobby'])
