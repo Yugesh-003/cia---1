c6cde777597171146dfa674f99eb46f4fa728add
yugesh <a.yugesh2@gmail.com>
2025-01-23 14:48:51 +0530

    changed few prgms

--- list-comp/7.py
+++
@@ -1,4 +0,0 @@
-#7.Create a list of strings with vowels replaced by asterisks
-words = ["apple", "banana", "cherry"]
-a= [''.join('*' if char in 'aeiouAEIOU' else char for char in word) for word in words]
-print(a)
